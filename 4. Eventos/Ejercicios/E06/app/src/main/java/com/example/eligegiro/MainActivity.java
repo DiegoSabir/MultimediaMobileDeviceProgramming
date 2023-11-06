73a89dd4c5e274f16a52d33d0f60455c942c4986
Diego Ribas González <diegoribas59@gmail.com>
2023-11-06 13:59:39 +0100

    update

---
+++ 4. Eventos/Ejercicios/E06/app/src/main/java/com/example/eligegiro/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.eligegiro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
