b1b4c936df608e03b890f35ff827cea97529f490
Diego Ribas González <90776736+DiegoSabir@users.noreply.github.com>
2023-11-12 19:25:33 +0100

    update

--- 4. Eventos/Ejercicios/E10/app/build.gradle.kts
+++ 4. Eventos/Ejercicios/E10/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.cambiodemoneda"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cambiodemoneda"
--- 4. Eventos/Ejercicios/E10/app/src/main/java/com/example/cambiodemoneda/MainActivity.java
+++ 4. Eventos/Ejercicios/E10/app/src/main/java/com/example/cambiodemoneda/MainActivity.java
@@ -3,12 +3,77 @@ package com.example.cambiodemoneda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //Atributes
+    private EditText etValor;
+    private RadioGroup rgConversion;
+    private LinearLayout llCantidad;
+    private Button btnCambiar;
+    private TextView txtFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.inits();
+    }
+
+    private void inits() {
+        //Edit Text
+        this.etValor = findViewById(R.id.etValor);
+
+        //Radio Groups
+        this.rgConversion = findViewById(R.id.rgConversion);
+
+        //LinearLayouts
+        this.llCantidad = findViewById(R.id.llCantidad);
+
+        //Buttons
+        this.btnCambiar = findViewById(R.id.btnCambiar);
+
+        this.btnCambiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    double valor = Double.parseDouble(etValor.getText().toString());
+                    conversorMoneda(valor);
+
+                }
+                catch (Exception ex) {
+                    Toast.makeText(MainActivity.this, "Introduzca un valor válido.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //TextViews
+        this.txtFinal = findViewById(R.id.txtFinal);
+
+        //Defaults
+        this.llCantidad.setVisibility(View.GONE);
+    }
+
+    private void conversorMoneda(double valor) {
+        double tasa = 166.386;
+        RadioButton selectedRB = findViewById(rgConversion.getCheckedRadioButtonId());
+
+        if (selectedRB.getId() == R.id.rbEurPts) {
+            double resultado = valor * tasa;
+            txtFinal.setText(valor + " euros equivalen a " + resultado + " pesetas.");
+        }
+        else {
+            double resultado = valor / tasa;
+            txtFinal.setText(valor + " pesetas equivalen a " + resultado + " euros.");
+        }
     }
 }
