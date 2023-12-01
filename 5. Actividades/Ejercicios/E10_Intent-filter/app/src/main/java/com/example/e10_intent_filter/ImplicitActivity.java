228c87a48906886f1c4de331db880af712d07c35
Diego Ribas González <diegoribas59@gmail.com>
2023-12-01 12:55:17 +0100

    update

--- 5. Actividades/Ejercicios/E10_Intent-filter/app/src/main/java/com/example/e10_intent_filter/ImplicitActivity.java
+++ 5. Actividades/Ejercicios/E10_Intent-filter/app/src/main/java/com/example/e10_intent_filter/ImplicitActivity.java
@@ -1,4 +1,39 @@
 package com.example.e10_intent_filter;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
 public class ImplicitActivity {
+
+
+
+
+
+
+
+
+
+
+
+
+
+    protected  void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_implicit);
+            webView = find.ViewById(R.id.webview_explicit);
+
+            //============================================
+            //WebSettings ws = webView.getSettings();
+            //ws.setJavaScriptEnabled(true);
+            // habilitar javascript para las paginas que lo necesitan
+            //============================================
+
+            Intent intent = getIntent();
+            Uri uriRecibida = intent.getData();
+            webView.loadUrl(uriRecibida.toString()); //loadURL NECESITA string
+
+            //"This shite requieres JavaScript and Cookies to be enabled.
+
+    }
 }
